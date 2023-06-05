5e4cbb0cc8c4b0fa70a58e6f2df294b670d51e92
Rafal Brzoska <rafal@brzoska.net.pl>
2023-06-05 12:00:43 +0200

    Higher order operators

--- src/app/rx/rx-test/rx-test.component.ts
+++ src/app/rx/rx-test/rx-test.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Observable, catchError, debounceTime, from, fromEvent, map, of, throwError } from 'rxjs';
+import { Observable, catchError, combineLatest, concatMap, debounceTime, delay, exhaustMap, forkJoin, from, fromEvent, map, merge, mergeMap, of, switchMap, tap, throwError, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'app-rx-test',
@@ -8,6 +9,7 @@ import { Observable, catchError, debounceTime, from, fromEvent, map, of, throwEr
 })
 export class RxTestComponent {
   @ViewChild('myInput', { static: true }) myInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('myButton', { static: true }) myButton!: ElementRef<HTMLButtonElement>;
 
   obs$ = new Observable(obs => {
     setTimeout(() => {
@@ -24,7 +26,10 @@ export class RxTestComponent {
   ofObs$ = of(1, 2, 3, 4, 5);
   fromObs$ = from([1,2,3,4,5])
   fromEventObs$!: Observable<any>;
+  clickObs$!: Observable<any>;
 
+
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     this.obs$.pipe(
       catchError(err => {
@@ -35,9 +40,37 @@ export class RxTestComponent {
     this.ofObs$.subscribe(val => console.log(val));
     this.fromEventObs$ = fromEvent(this.myInput.nativeElement, 'input').pipe(
       debounceTime(500),
-      map((input: any) => input.target.value),
+      map((input: any) => input.target.value)),
 
-    )
-    this.fromEventObs$.subscribe(v => console.log(v))
+    // this.fromEventObs$.pipe(
+    //   concatMap((val) => this.http.get<any>('http://localhost:3000/user', {
+    // params: { search: val}
+    // })))
+
+    this.clickObs$ =  fromEvent(this.myButton.nativeElement, 'click')
+    // this.clickObs$.subscribe(() => this.http.get<any>('http://localhost:3000/user').subscribe())
+
+    // const subs = this.http.get<any>('http://localhost:3000/user').subscribe();
+    // setTimeout(() => subs.unsubscribe(), 500)
+
+    // this.clickObs$.pipe(
+    //     switchMap((val) => this.http.get<any>('http://localhost:3000/user')),
+    //   ).subscribe(val => console.log(val))
+
+    // this.clickObs$.pipe(
+    //     concatMap((val) => this.http.get<any>('http://localhost:3000/user')),
+    //   ).subscribe(val => console.log(val))
+
+    // this.clickObs$.pipe(
+    //     mergeMap((val) => this.http.get<any>('http://localhost:3000/user')),
+    //   ).subscribe(val => console.log(val))
+
+    // this.clickObs$.pipe(
+    //     exhaustMap((val) => this.http.get<any>('http://localhost:3000/user')),
+    //   ).subscribe(val => console.log(val))
+
+    // merge(this.clickObs$, this.fromEventObs$).subscribe(console.log);
+    combineLatest([this.clickObs$, this.fromEventObs$]).subscribe(console.log)
+    // forkJoin([this.http.get<any>('http://localhost:3000/user').pipe(delay(2000)), this.http.get<any>('http://localhost:3000/user')]).subscribe(v => console.log(v))
   }
 }
