68cac38432f2a1d5abaa7b77ada101bf37ee2fca
Rafal Brzoska <rafal@brzoska.net.pl>
2023-06-06 09:39:06 +0200

    sidenav  + toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
