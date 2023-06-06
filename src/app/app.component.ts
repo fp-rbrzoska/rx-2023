90f2324c0fc7fc397de06af3fa259e87c6c2ce17
Rafal Brzoska <rafal@brzoska.net.pl>
2023-06-06 10:11:50 +0200

    sidenav open

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { AppStateService } from './app-state.service';
 import { Observable } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav
   title = 'rx-proj';
   pending$!: Observable<boolean>;
   constructor(private _appStateService: AppStateService ) {
     this.pending$ = _appStateService.pending$;
+  }
+
+  ngAfterViewInit(): void {
+      setTimeout(() => this.sidenav.close());
+
   }
 }
